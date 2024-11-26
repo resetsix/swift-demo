7aca872ce937e298e2dd1a2d5e13b7399e581b4e
一闪一闪亮晶晶 <resetsix223@gmail.com>
2024-11-26 10:50:17 +0800

    Initial Commit

---
+++ swift-basic/swift_basicApp.swift
@@ -0,0 +1,17 @@
+//
+//  swift_basicApp.swift
+//  swift-basic
+//
+//  Created by resetsix on 2024/11/26.
+//
+
+import SwiftUI
+
+@main
+struct swift_basicApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
